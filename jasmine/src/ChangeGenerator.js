a177e62b176c2afa3db083c5e3d9fce7011ae4a4
Noel Vock <noel1uk@gmail.com>
2018-05-15 13:44:53 +0100

    feat: `setChangeOwed` sets the changeOwed property

--- jasmine/src/ChangeGenerator.js
+++ jasmine/src/ChangeGenerator.js
@@ -1,11 +1,17 @@
 class ChangeGenerator {
-
+  
   change() {
     return '1 x 1p';
   }
-
+  
   turnToNumber(stringValue) {
     let number = parseInt(stringValue.slice(0, -1));
     return number;
   }
+  
+  setChangeOwed(valueRecieved, valueReturned) {
+    this.changeOwed = valueRecieved - valueReturned;
+    console.log(this);
+  }
+
 }
