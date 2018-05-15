836597b45728ad84a4ee701a78b46ec0e8e27165
Noel Vock <noel1uk@gmail.com>
2018-05-15 13:26:44 +0100

    test: `turnToNumber` removes the p from input

--- jasmine/spec/ChangeGeneratorSpec.js
+++ jasmine/spec/ChangeGeneratorSpec.js
@@ -1,10 +1,15 @@
 describe('ChangeGenerator', function() {
+  let cg = new ChangeGenerator();
 
   describe('.change', function() {
-    let cg = new ChangeGenerator();
     
     it('returns 1 x 1p when given 2p and 1p', function() {
       expect(cg.change('2p', '1p')).toEqual('1 x 1p');
+    });
+  });
+  describe('.turnToNumber', function() {
+    it('removes the last character of the string', function() {
+      expect(cg.turnToNumber('1p')).toEqual(1);
     });
   });
 });
